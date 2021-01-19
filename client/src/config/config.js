b3f40b74511f58f793a49273c0f69c3cafcbbfb5
unknown <danco24000@gmail.com>
2021-01-19 12:07:48 +0200

    added localhost to config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Server_URL: process.env.SERVER_URL || "https://dumplings-tlv-gal.herokuapp.com",
+    Server_URL: process.env.SERVER_URL || "http://localhost:8080",
     Order_Properties: [
         {
             id: "orderDate",
